85980dc997a64cc891854bda8cd2c728c7512119
Aleksey Chendemerov <merkyzeze@tutanota.com>
2021-02-23 17:56:50 +0300

    Added templates for functions for Player class.

--- src_new/Player.py
+++ src_new/Player.py
@@ -1,5 +1,13 @@
 from PyQt5.QtWidgets import QWidget
 
-class Player(QWidget):
-	def __init__(self, parent = None):
-		super(Player, self).__init__(parent)
+from Object import Object
+
+class Player(Object):
+	def __init__(self, color, height, width, pos_x, pos_y, parent = None):
+		super(Player, self).__init__(color, height, width, pos_x, pos_y, parent)
+
+	def move_up(self):
+		pass
+
+	def move_down(self):
+		pass
